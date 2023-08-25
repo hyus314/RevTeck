189720b5fe3bf3d8595d9049939246985a5d21b0
Hyusein Osmanov <osmanovhyus@gmail.com>
2023-08-25 22:19:07 +0300

    Updated Home Page

---
+++ RevTech/wwwroot/js/site/homePage.js
@@ -0,0 +1,48 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const config = {
+        step: 2, 
+        resizeDebounceTime: 300,
+    };
+
+    let position = 0;
+    let totalWidth = 0;
+    let resizeTimeout;
+    const imageList = document.getElementById("imageList");
+    const images = Array.from(imageList.children);
+
+    function calculateTotalWidth() {
+        totalWidth = images.reduce((acc, img) => acc + img.clientWidth, 0);
+    }
+
+    function cloneImages() {
+        images.forEach((img) => {
+            const clone = img.cloneNode(true);
+            imageList.appendChild(clone);
+        });
+    }
+
+    function moveImages() {
+        position -= config.step;
+
+        if (-position >= totalWidth) {
+            position += totalWidth;
+        }
+
+        imageList.style.transform = `translateX(${position}px)`;
+        requestAnimationFrame(moveImages);
+    }
+
+    function handleResize() {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(() => {
+            calculateTotalWidth();
+        }, config.resizeDebounceTime);
+    }
+
+    calculateTotalWidth();
+    cloneImages();
+
+    window.addEventListener('resize', handleResize);
+
+    requestAnimationFrame(moveImages);
+});
